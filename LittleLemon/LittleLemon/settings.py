ab2ae1134405a00b958a6215871045037128375e
luckyalade <luckyalade309@gmail.com>
2024-01-05 22:42:43 +0100

    configured database schemas, populated them

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'LittleLemon',
     'Restaurant',
 ]
 
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Elevator309$',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+  Name = models.CharField(max_length=255)
+  No_of_guests = models.IntegerField()
+  BookingDate = models.DateTimeField()
+
+  def __str__(self):
+    return self.Name
+
+
+class Menu(models.Model):
+  Title = models.CharField(max_length=255)
+  Price = models.DecimalField(max_digits=10, decimal_places=2)
+  Inventory = models.IntegerField()
+
+  def __str__(self):
+    return self.Title
