dccb75e383d0f50bb8e49b75325f22e4289d79be
luckyalade <luckyalade309@gmail.com>
2024-01-07 20:27:22 +0100

    included some comments

--- LittleLemon/Restaurant/urls.py
+++ LittleLemon/Restaurant/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 
-
+# Define the URL patterns for the app
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemView.as_view(), name='menu-list'),
--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -5,27 +5,29 @@ from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+# Define the index view
 def index(request):
   return render(request, 'index.html', {})
 
-
+# Define the menu item view
 class MenuItemView(generics.ListCreateAPIView):
   queryset = Menu.objects.all()
   serializer_class = MenuItemSerializer
   permission_classes = [IsAuthenticated]
 
+  # Define the index view for the menu items
   def index(request):
     print(request.user)
     return render(request, 'index.html', {})
 
 
-
+# Define the single menu item view
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
   queryset = Menu.objects.all()
   serializer_class = MenuItemSerializer
   permission_classes = [IsAuthenticated]
 
-
+# Define the booking view set
 class BookingViewSet(viewsets.ModelViewSet):
   queryset = Booking.objects.all()
   serializer_class = BookingSerializer
